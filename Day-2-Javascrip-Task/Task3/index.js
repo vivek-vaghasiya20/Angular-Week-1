a019d9efeb09d0185bf9bacc77163ebfa6e31658
vivek-vaghasiya20 <vivek.vaghasiya@bacancy.com>
2024-03-18 13:14:36 +0530

    update to-do

--- Day-2-Javascrip-Task/Task3/index.js
+++ Day-2-Javascrip-Task/Task3/index.js
@@ -1,11 +1,32 @@
-addtask = () => {
-  const taskinput = document.getElementsByClassName("task-input");
-  const tasklist = document.getElementsByClassName("task-list");
+addTask = () => {
+  const taskInput = document.querySelector(".task-input");
+  const taskList = document.querySelector(".task-list");
 
-  const taskitem = taskinput.value;
+  const taskItem = taskInput.value.trim();
 
-  const newtaskadd = document.createElement("li");
-  newtaskadd.textContent = taskitem;
+  if (taskItem !== "") {
+    const newTaskAdd = document.createElement("li");
 
-  tasklist.appendChild(newtaskadd);
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", () => {
+      if (checkbox.checked) {
+        newTaskAdd.style.textDecoration = "line-through";
+      } else {
+        newTaskAdd.style.textDecoration = "none";
+      }
+    });
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.addEventListener("click", () => {
+      taskList.removeChild(newTaskAdd);
+    });
+
+    newTaskAdd.appendChild(checkbox);
+    newTaskAdd.appendChild(document.createTextNode(taskItem));
+    newTaskAdd.appendChild(removeButton);
+    taskList.appendChild(newTaskAdd);
+  }
+  taskInput.value = "";
 };
